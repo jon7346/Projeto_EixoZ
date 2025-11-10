39cbcf254ce150c5c7ff3145808102adf293f399
Henrique Dominguez Fernandes <henriquedominguez12@gmail.com>
2025-11-10 13:27:57 -0300

    Commit corrigido

--- Projeto_EixoZ/Views/frmCadVendedor.cs
+++ Projeto_EixoZ/Views/frmCadVendedor.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Projeto_EixoZ.Controllers;
+using Projeto_EixoZ.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,119 @@ namespace Projeto_EixoZ.Views
 {
     public partial class frmCadVendedor: Form
     {
-        public frmCadVendedor()
+        VendedorController vendedorController = new VendedorController();
+        public frmCadVendedor(int Acao = 1, Vendedor vendedor = null)
         {
             InitializeComponent();
+            DefinirModoTela(Acao, vendedor);
+        }
+
+        private void DefinirModoTela(int acao, Vendedor vendedor)
+        {
+            if (vendedor != null)
+                CarregarDados(vendedor);
+
+            // Define o título da janela com base na ação
+            switch (acao)
+            {
+                case 1:
+                    this.Text = "Cadastrar Vendedor";
+                    break;
+                case 2:
+                    this.Text = "Editar Vendedor";
+                    break;
+                case 3:
+                    this.Text = "Visualizar Vendedor";
+                    DesativarCampos();
+                    break;
+            }
+
+
+        }
+        void DesativarCampos()
+        {
+            // Desativa os campos para visualização apenas
+            txtIDCadVendedor.ReadOnly = true;
+            txtNomeCadVendedor.ReadOnly = true;
+            txtEmailCadVendedor.ReadOnly = true;
+            txtSenhaCadVendedor.ReadOnly = true;
+            txtEnderecoCadVendedor.ReadOnly = true;
+            txtIdadeCadVendedor.ReadOnly = true;
+
+            // Oculta os botões de salvar e limpar
+            btnSalvarCadVendedor.Visible = false;
+            btnCancelarCadVendedor.Visible = true;
+            btnCancelarCadVendedor.Text = "Fechar";
+        }
+
+        void CarregarDados(Vendedor vendedor)
+        {
+            //carrega os dados que forem inseridos nos campos de cliente
+            txtIDCadVendedor.Text = vendedor.IdVendedor.ToString();
+            txtNomeCadVendedor.Text = vendedor.Nome.ToString();
+            txtEmailCadVendedor.Text = vendedor.Email.ToString();
+            txtSenhaCadVendedor.Text = vendedor.Senha.ToString();
+            txtEnderecoCadVendedor.Text = vendedor.Endereco.ToString();
+            txtIdadeCadVendedor.Text = vendedor.Idade.ToString();
+        }
+
+        private void btnSalvarCadVendedor_Click(object sender, EventArgs e)
+        {
+
+            Vendedor vendedor = new Vendedor();
+
+            vendedor.Nome = txtNomeCadVendedor.Text;
+            vendedor.Email = txtEmailCadVendedor.Text;
+            vendedor.Senha = txtSenhaCadVendedor.Text;
+
+            int retorno = 0;
+            if (txtIDCadVendedor.Text == "")
+                retorno = vendedorController.Inserir(vendedor);
+            else
+            {
+
+                vendedor.IdVendedor = int.Parse(txtIDCadVendedor.Text);
+                retorno = vendedorController.Alterar(vendedor);
+            }
+
+            if (retorno > 0)
+            {
+                MessageBox.Show(
+                    "Cadastro salvo com sucesso!",
+                    "Informação!", MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+
+                DialogResult = DialogResult.OK;
+            }
+            else
+                MessageBox.Show(
+                    "Falha ao salvar o cadastro!",
+                    "Atenção!", MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+        }
+
+        private void btnCancelarCadVendedor_Click(object sender, EventArgs e)
+        {
+            if (btnSalvarCadVendedor.Visible == false)
+            {
+                DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            else
+            {
+                if (MessageBox.Show(
+                    "Deseja realmente sair?" + Environment.NewLine + Environment.NewLine +
+                    "Ao Sair os dados alterados serão descartados!",
+                    "Confirmação!",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question,
+                    MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+                {
+                    DialogResult = DialogResult.Cancel;
+                    this.Close();
+                }
+
+            }
         }
     }
 }
--- Projeto_EixoZ/Views/frmCadastroCliente.cs
+++ Projeto_EixoZ/Views/frmCadastroCliente.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Projeto_EixoZ.Controllers;
+using Projeto_EixoZ.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,120 @@ namespace Projeto_EixoZ.Views
 {
     public partial class frmCadastroCliente: Form
     {
-        public frmCadastroCliente()
+        //criando instancia com a controller
+        ClienteController clienteController = new ClienteController();
+        public frmCadastroCliente(int Acao = 1, Cliente cliente = null)
         {
             InitializeComponent();
+            DefinirModoTela(Acao, cliente);
+        }
+
+        private void DefinirModoTela(int acao, Cliente cliente)
+        {
+            if (cliente != null)
+                CarregarDados(cliente);
+
+            // Define o título da janela com base na ação
+            switch (acao)
+            {
+                case 1:
+                    this.Text = "Cadastrar Cliente";
+                    break;
+                case 2:
+                    this.Text = "Editar Cliente";
+                    break;
+                case 3:
+                    this.Text = "Visualizar Cliente";
+                    DesativarCampos();
+                    break;
+            }
+
+
+        }
+        void DesativarCampos()
+        {
+            // Desativa os campos para visualização apenas
+            txtIDCadCliente.ReadOnly = true;
+            txtNomeCadCliente.ReadOnly = true;
+            txtEmailCadCliente.ReadOnly = true;
+            txtSenhaCadCliente.ReadOnly = true;
+            txtEnderecoCadCliente.ReadOnly = true;
+            txtIdadeCadCliente.ReadOnly = true;
+
+            // Oculta os botões de salvar e limpar
+            btnSalvarCadCliente.Visible = false;
+            btnCancelarCadCliente.Visible = true;
+            btnCancelarCadCliente.Text = "Fechar";
+        }
+
+        void CarregarDados(Cliente cliente)
+        {
+            //carrega os dados que forem inseridos nos campos de cliente
+            txtIDCadCliente.Text = cliente.ClienteId.ToString();
+            txtNomeCadCliente.Text = cliente.Nome.ToString();
+            txtEmailCadCliente.Text = cliente.Email.ToString();
+            txtSenhaCadCliente.Text = cliente.Senha.ToString();
+            txtEnderecoCadCliente.Text = cliente.Endereco.ToString();
+            txtIdadeCadCliente.Text = cliente.Idade.ToString();
+        }
+
+        private void btnSalvarCadCliente_Click(object sender, EventArgs e)
+        {
+
+            Cliente cliente = new Cliente();
+
+            cliente.Nome = txtNomeCadCliente.Text;
+            cliente.Email = txtEmailCadCliente.Text;
+            cliente.Senha = txtSenhaCadCliente.Text;
+
+            int retorno = 0;
+            if (txtIDCadCliente.Text == "")
+                retorno = clienteController.Inserir(cliente);
+            else
+            {
+
+                cliente.ClienteId = int.Parse(txtIDCadCliente.Text);
+                retorno = clienteController.Alterar(cliente);
+            }
+
+            if (retorno > 0)
+            {
+                MessageBox.Show(
+                    "Cadastro salvo com sucesso!",
+                    "Informação!", MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+
+                DialogResult = DialogResult.OK;
+            }
+            else
+                MessageBox.Show(
+                    "Falha ao salvar o cadastro!",
+                    "Atenção!", MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+        }
+
+        private void btnCancelarCadCliente_Click(object sender, EventArgs e)
+        {
+            if (btnSalvarCadCliente.Visible == false)
+            {
+                DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            else
+            {
+                if (MessageBox.Show(
+                    "Deseja realmente sair?" + Environment.NewLine + Environment.NewLine +
+                    "Ao Sair os dados alterados serão descartados!",
+                    "Confirmação!",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question,
+                    MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+                {
+                    DialogResult = DialogResult.Cancel;
+                    this.Close();
+                }
+
+            }
         }
     }
 }
